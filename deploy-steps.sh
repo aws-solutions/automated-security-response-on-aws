029a0386d3a6f3ddde594b37c46a3383a595dba0
Asitha Jayathilake <asithagihan03@gmail.com>
2022-09-20 14:27:19 +0530

    Delete deploy-steps.sh
    Remove unwanted file

--- deploy-steps.sh
+++
@@ -1,9 +0,0 @@
-./build-s3-dist.sh -b 5pillars-playbooks -v 1.4.2.1
-./upload_s3_dist.sh ap-southeast-2
-./upload_s3_dist.sh ap-northeast-1
-./upload_s3_dist.sh us-east-1
-./upload_s3_dist.sh us-east-2
-./upload_s3_dist.sh us-west-1
-./upload_s3_dist.sh us-west-2
-aws s3 sync ./global-s3-assets s3://5pillars-playbooks-reference/aws-security-hub-automated-response-and-remediation/latest/
-aws s3 sync ./global-s3-assets s3://5pillars-playbooks-reference/aws-security-hub-automated-response-and-remediation/v1.4.2.1/
