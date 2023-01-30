cea6e49b344dca3a28f385a04b321fbde6ac0376
Max Granat <maxgran@amazon.com>
2023-01-30 09:50:05 -0500

    Cleanup param class

--- source/lib/admin-account-param.ts
+++ source/lib/admin-account-param.ts
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import * as cdk from 'aws-cdk-lib';
+import { CfnParameter } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export default class AdminAccountParam extends Construct {
@@ -9,14 +9,14 @@ export default class AdminAccountParam extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    const validAwsAccount = '^\\d{12}$';
-
-    const param = new cdk.CfnParameter(this, 'Admin Account Number', {
+    const accountIdRegex = /^\d{12}$/;
+    const param = new CfnParameter(this, 'Admin Account Number', {
       description: 'Admin account number',
       type: 'String',
-      allowedPattern: validAwsAccount,
+      allowedPattern: accountIdRegex.source,
     });
     param.overrideLogicalId(`SecHubAdminAccount`);
+
     this.value = param.valueAsString;
   }
 }
