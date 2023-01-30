9f2c33dec35b3bf450e319defe8115d1feca3514
Max Granat <maxgran@amazon.com>
2023-01-30 09:12:15 -0500

    Rename symbols related to admin account parameter

--- source/lib/admin_account_parm.test.ts
+++ source/lib/admin_account_parm.test.ts
@@ -1,19 +1,21 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { App, DefaultStackSynthesizer, Stack } from 'aws-cdk-lib';
+import { App, Aspects, DefaultStackSynthesizer, Stack } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
+import { AwsSolutionsChecks } from 'cdk-nag';
 import { AdminAccountParm } from './admin_account_parm-construct';
 
-function createAdminAccountParm(): Stack {
+function createAdminAccountParameterStack(): Stack {
   const app = new App();
-  const stack = new Stack(app, 'testStack', {
+  const stack = new Stack(app, 'AdminAccountParameterStack', {
+    analyticsReporting: false,
     synthesizer: new DefaultStackSynthesizer({ generateBootstrapVersionRule: false }),
-    stackName: 'testStack',
   });
-  new AdminAccountParm(stack, 'roles');
+  Aspects.of(stack).add(new AwsSolutionsChecks({ verbose: true }));
+  new AdminAccountParm(stack, 'AdminAccountParameter');
   return stack;
 }
 
 test('AdminParm Test Stack', () => {
-  expect(Template.fromStack(createAdminAccountParm())).toMatchSnapshot();
+  expect(Template.fromStack(createAdminAccountParameterStack())).toMatchSnapshot();
 });
