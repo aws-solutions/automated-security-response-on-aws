a9f505fd346bda3878823ab7ebcb17d8eed4d2c2
Max Granat <maxgran@amazon.com>
2023-02-09 15:15:13 -0500

    Batch documents in groups

--- source/solution_deploy/lib/member/wait-provider.ts
+++ source/solution_deploy/lib/member/wait-provider.ts
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { createHash } from 'crypto';
-import { CustomResource, Duration, Stack } from 'aws-cdk-lib';
+import { createHash, Hash } from 'crypto';
+import { CfnCustomResource, CustomResource, Duration, Stack } from 'aws-cdk-lib';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { CfnDocument } from 'aws-cdk-lib/aws-ssm';
@@ -72,7 +72,53 @@ export class SsmDocumentWaitProvider extends Construct {
     }
   }
 
+  private currentWaitResource: CustomResource | undefined;
+  private hash: Hash | undefined;
+  private readonly maxBatchedDocuments = 5;
+  private batchedDocuments = 0;
+
   createWaitResource(scope: Construct, id: string, props: SsmDocumentWaitResourceProps): CustomResource {
+    if (!this.currentWaitResource || !this.hash) {
+      // no need for cryptographic security, we just need a property that indicates when the underlying document changes
+      this.hash = createHash('sha256');
+      this.currentWaitResource = new CustomResource(scope, id, {
+        serviceToken: this.serviceToken,
+        properties: {
+          CreateIntervalSeconds: 1,
+          UpdateIntervalSeconds: 1,
+          DeleteIntervalSeconds: 0.5,
+        },
+        resourceType: 'Custom::Wait',
+      });
+    }
+
+    const relevantProperties: string[] = [
+      props.document.name,
+      props.document.documentFormat,
+      props.document.documentType,
+      props.document.content,
+      props.document.updateMethod,
+    ];
+    this.hash.update(JSON.stringify(relevantProperties));
+
+    const waitResource = this.currentWaitResource;
+
+    (waitResource.node.defaultChild as CfnCustomResource).addPropertyOverride(
+      'DocumentPropertiesHash',
+      this.hash.copy().digest('hex') // multiple calls to digest not allowed, copy to create rolling hash
+    );
+
+    ++this.batchedDocuments;
+
+    if (this.batchedDocuments >= this.maxBatchedDocuments) {
+      this.batchedDocuments = 0;
+      this.currentWaitResource = undefined;
+    }
+
+    return waitResource;
+  }
+
+  createWaitResourceOld(scope: Construct, id: string, props: SsmDocumentWaitResourceProps): CustomResource {
     const relevantProperties: string[] = [
       props.document.name,
       props.document.documentFormat,
