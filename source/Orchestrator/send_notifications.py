36e24a763f819054da9b4827d9f05ebb5dc5879b
Tim Mekari <mekart@amazon.com>
2023-02-21 11:54:59 -0800

    Fixed typo with set

--- source/Orchestrator/send_notifications.py
+++ source/Orchestrator/send_notifications.py
@@ -111,7 +111,7 @@ def lambda_handler(event, _):
         notification.severity = 'ERROR'
         notification.send_to_sns = True
 
-    elif event['Notification']['State'].upper() == {'WRONGSTANDARD' or 'LAMBDAERROR'}:
+    elif event['Notification']['State'].upper() == {'WRONGSTANDARD', 'LAMBDAERROR'}:
         notification = sechub_findings.SHARRNotification('SHARR',AWS_REGION, None)
         notification.severity = 'ERROR'
 
