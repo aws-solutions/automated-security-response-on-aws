1e33f8226c78985377cd935bd0b4a35aca62e13a
Tim Mekari <mekart@amazon.com>
2023-02-21 12:11:51 -0800

    Changed to in

--- source/Orchestrator/send_notifications.py
+++ source/Orchestrator/send_notifications.py
@@ -111,7 +111,7 @@ def lambda_handler(event, _):
         notification.severity = 'ERROR'
         notification.send_to_sns = True
 
-    elif event['Notification']['State'].upper() == {'WRONGSTANDARD', 'LAMBDAERROR'}:
+    elif event['Notification']['State'].upper() in {'WRONGSTANDARD', 'LAMBDAERROR'}:
         notification = sechub_findings.SHARRNotification('SHARR',AWS_REGION, None)
         notification.severity = 'ERROR'
 
