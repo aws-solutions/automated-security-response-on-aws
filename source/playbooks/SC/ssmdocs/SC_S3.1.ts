f03404363ade568006b06f74cfb3ca05201ed1d3
Tim Mekari <35642342+tmekari@users.noreply.github.com>
2023-03-08 08:55:28 -0800

    Seperated CIS 2.1.5 into two remediations (#137)

--- source/playbooks/SC/ssmdocs/SC_S3.1.ts
+++ source/playbooks/SC/ssmdocs/SC_S3.1.ts
@@ -9,7 +9,7 @@ export function createControlRunbook(scope: Construct, id: string, props: Playbo
   return new ConfigureS3PublicAccessBlockDocument(scope, id, { ...props, controlId: 'S3.1' });
 }
 
-class ConfigureS3PublicAccessBlockDocument extends ControlRunbookDocument {
+export class ConfigureS3PublicAccessBlockDocument extends ControlRunbookDocument {
   constructor(scope: Construct, id: string, props: ControlRunbookProps) {
     super(scope, id, {
       ...props,
