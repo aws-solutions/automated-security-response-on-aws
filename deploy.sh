58293198a878d63cf3711b691e58ad823329c910
Zultan Holder <zultan@6pillars.io>
2024-06-19 03:52:09 +0000

    Update version, add docker compose

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 cd deployment
 chmod +x ./build-s3-dist.sh
-./build-s3-dist.sh -b 5pillars-prod-playbooks -c v2.0.1.10
-./build-s3-dist.sh -b 5pillars-prod-playbooks -v v2.0.1.10
+./build-s3-dist.sh -b 5pillars-prod-playbooks -c v2.0.1.11
+./build-s3-dist.sh -b 5pillars-prod-playbooks -v v2.0.1.11
 aws s3 sync ./global-s3-assets s3://5pillars-prod-playbooks-reference/aws-security-hub-automated-response-and-remediation/latest/
-aws s3 sync ./global-s3-assets s3://5pillars-prod-playbooks-reference/aws-security-hub-automated-response-and-remediation/v2.0.1.10/ # replace this with correct versio
+aws s3 sync ./global-s3-assets s3://5pillars-prod-playbooks-reference/aws-security-hub-automated-response-and-remediation/v2.0.1.11/ # replace this with correct versio
 chmod +x ./upload-s3-dist.sh
 ./upload-s3-dist.sh ap-southeast-2
 ./upload-s3-dist.sh ap-northeast-1
